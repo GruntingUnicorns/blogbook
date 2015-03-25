a37dec4c1376353d80e866ff0dc42cdd787bc30e
Kate <katebeavis84@gmail.com>
2015-03-25 11:32:17 +0000

    Created addlinkctrl and protractor test

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -9,9 +9,14 @@ describe('blogbook homepage', function() {
 
   it('should show a blog post', function() {
     browser.get('http://localhost:3000/');
-    // var list = element.all(by.css('.items li'));
-    // expect(list.get(1).getText()).toBe("blog");
-    expect(element(by.css('li')).getText()).toBe("kaprianofox")
+    expect(element(by.binding('card.Title')).getText()).toBe("Hello World")
+  });
+
+  it("should take a link", function() {
+    browser.get('http://localhost:3000/');
+    element(by.id('addlink')).sendKeys('http://katebeavis.wordpress.com');
+    element(by.id('submitButton')).click();
+    expect(element(by.binding('link')).getText()).toEqual('http://katebeavis.wordpress.com');
   });
 
 });
