e162eb2d7b11dbf645a2430dd3d61f0b4014c5b3
tommasobratto <tbratto22@gmail.com>
2015-03-27 09:57:16 +0000

    initialised new branch

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -1,1 +1,1 @@
-angular.module("BlogBook", [])
+angular.module("BlogBook", []);
--- public/js/streamController.js
+++ public/js/streamController.js
@@ -1,11 +1,25 @@
 var streamController = function($scope, WPCard) {
   $scope.cards = [];
   $scope.filters = {}
-  $scope.blogs = ['katebeavis.wordpress.com', 'joejknowles.wordpress.com', 'detachedhead.wordpress.com', 'thegeekleapforward.wordpress.com', 'en.blog.wordpress.com'];
+  $scope.blogs = [
+    'katebeavis.wordpress.com', 
+    'joejknowles.wordpress.com', 
+    'detachedhead.wordpress.com', 
+    'thegeekleapforward.wordpress.com', 
+    'em01blog.wordpress.com', 
+    'niccipell.wordpress.com', 
+    'sevenhoursbehind.wordpress.com', 
+    'makingetc.wordpress.com', 
+    'webcub.wordpress.com', 
+    'startingupupandaway.wordpress.com', 
+    'srikanthkunkulagunta.wordpress.com', 
+    'ilearntocode.wordpress.com', 
+    'technotzen.wordpress.com', 
+    'meganfolsom.wordpress.com'
+    ];
 
   $scope.addWPCard = function(blog){
     $scope.cards.push(new WPCard(blog, 0));
-    $scope.cards.push(new WPCard(blog, 1));
   };
 
  $scope.allPosts = function(blog){
@@ -20,12 +34,14 @@ var streamController = function($scope, WPCard) {
     }
   };
 
-  $scope.fillPage();
-
-
   $scope.setFilter = function(index){
     $scope.filters = {author: $scope.cards[index].author}
   };
+
+  angular.element(document).ready(function() {
+    $scope.fillPage();
+  });
+
 };
 
 angular
